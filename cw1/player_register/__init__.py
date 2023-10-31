9c20bbb8f653260c6a9c3a2531920c122ca85a99
dz_paji <zhangqian2002@outlook.com>
2023-10-31 16:12:49 +0000

    implement player/register

---
+++ cw1/player_register/__init__.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from shared.Player import Player
+import json
+
+from azure.functions import HttpRequest, HttpResponse
+from azure.cosmos import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError, CosmosResourceExistsError, CosmosResourceNotFoundError
+
+ThisCosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+PlayerDB = ThisCosmos.get_database_client(os.environ['DatabaseName'])
+PlayerContainer = PlayerDB.get_container_client(os.environ['PlayerContainerName'])
+
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info('New register request. %s', req.get_json())
+    req_body = req.get_json()
+    name = req_body.get('username')
+    passwd = req_body.get('password')
+    player = Player(name = name, passwd = passwd)
+    flag = player.validation()
+
+    match flag:
+        case 0:
+            try:
+                logging.info('Creating new player %s', player.to_dict())
+                PlayerContainer.create_item(player.to_dict(), enable_automatic_id_generation=True)
+                return HttpResponse(body=json.dumps({"result" : True, "msg": "OK" }), mimetype="application/json")
+            except CosmosHttpResponseError as err:
+                body=json.dumps({"result": False, "msg": "Username already exists" })
+        case 1:
+            body = json.dumps({"result": False, "msg": "Username less than 4 characters or more than 14 characters"  } )
+            return HttpResponse(body=body, mimetype="application/json")
+        case 2:
+            body = json.dumps({"result": False, "msg": "Password less than 10 characters or more than 20 characters"  } )
+            return HttpResponse(body=body, mimetype="application/json")
---
+++ cw1/shared/Player.py
@@ -0,0 +1,24 @@
+class Player:
+
+    def __init__(self, name, passwd, games_played = 0, score = 0):
+        self.id = 0
+        self.name = name
+        self.passwd = passwd
+        self.games_played = games_played
+        self.score = score
+
+    def validation(self):
+        # implement this method
+        # return 0 for valid player
+        # return 1 for invalid user name
+        # return 2 for invalid password
+        if len(self.name) < 4 or len(self.name) > 14:
+            return 1
+        if len(self.passwd) < 10 or len(self.passwd) > 20:
+            return 2
+        
+        return 0
+
+    def to_dict(self):
+        return { "id": self.id, "username": self.name, "password": self.passwd, "games_played": self.games_played, "total_score": self.score}
+
